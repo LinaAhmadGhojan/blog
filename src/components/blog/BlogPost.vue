5139a4d651c6b9683fc4111b47d43ddd799d04cb
LinaAhmadGhojan <linaghojan2000@gmail.com>
2023-03-27 03:30:20 -0700

    blog

---
+++ src/components/blog/BlogPost.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="post-">
+        <h1>{{title}}</h1>
+        <p>{{description}}</p>
+    </div>
+<br>
+    <div>
+<!--         <img src="../../assets/image/post/1.jpg" alt="imagepost1" style="width: 100%;"  srcset="">
+ -->    </div>
+</template>
+
+<script>
+export default {
+    name:"post",
+    props:["title","description"]
+
+}
+</script>
+
+<style>
+.post-{
+    background-color: #8d9663;
+    padding: 20px;
+}
+</style>
---
+++ src/views/BlogView.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="class-blog">
+    <h1>{{pagename}}</h1>
+  </div>
+  <div class="row">
+   
+    <div class="col-md-6">
+      <div class="posts-area">
+        <BlogPost v-for="post in posts" v-bind:key="post.id" :title="post.title" :description="post.description" />
+      </div>
+    </div>
+
+    <div class="col-md-4">
+      <div class="posts-sild">
+       <BlogSlid/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BlogPost from '../components/blog/BlogPost.vue';
+import BlogSlid from '../components/blog/BlogSlid.vue';
+import JsonPost from '../../json/post.json'
+export default {
+  data: function()
+ { 
+  return {
+    pagename:"blog",
+    pagedescription:"This is an blog page",
+    posts:JsonPost,
+  } ;
+},
+name:"blog",
+components:{
+  BlogPost,BlogSlid
+}
+    
+  }
+
+
+</script>
+  <style>
+  @media (min-width: 1024px) {
+    .help {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+
+    .class-blog{
+      text-align: center;
+    }
+
+    .posts-area{
+      text-align: center;
+    
+      padding: 20px;
+
+    }
+
+    .posts-sild{
+      text-align: left;
+
+    }
+    
+  }
+  </style>
+  
